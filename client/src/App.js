5065b05a24577aab33d0c50f26776b822c871c3f
Kyra Henningson <kyrahenningson@hotmail.com>
2023-01-16 14:33:40 -0800

    add content to navbar and style

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Products from "./pages/Products/Products";
 import Product from "./pages/Product/Product";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import "./app.scss"
 
 const Layout = () => {
   return (
