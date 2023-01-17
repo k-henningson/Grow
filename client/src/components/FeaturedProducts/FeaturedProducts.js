1b64ec16370a0454b3e9ea4cefb1e9ed1bf74419
Kyra Henningson <kyrahenningson@hotmail.com>
2023-01-16 19:32:50 -0800

    add more images to featured products data

--- client/src/components/FeaturedProducts/FeaturedProducts.js
+++ client/src/components/FeaturedProducts/FeaturedProducts.js
@@ -5,46 +5,46 @@ export default function FeaturedProducts({ type }) {
   const data = [
     {
       id: 1,
-      img: "",
-      img2: "",
-      title: "",
+      img: "https://raw.githubusercontent.com/k-henningson/Grow/main/client/src/docs/GlobePastelMix.webp",
+      img2: "https://raw.githubusercontent.com/k-henningson/Grow/main/client/src/docs/GlobePastelMix2.webp",
+      title: "Globe Amaranth Pastel Mix",
       isNew: true,
-      oldPrice: 2,
-      price: 1,
+      oldPrice: 5.95,
+      price: 4.95,
     },
     {
       id: 2,
       img: "",
       img2: "",
-      title: "",
+      title: "Strawflower Silvery Rose",
       isNew: true,
-      oldPrice: 2,
-      price: 1,
+      oldPrice: 5.95,
+      price: 4.95,
     },
     {
       id: 3,
       img: "",
       img2: "",
-      title: "",
+      title: "Breadseed Poppy Frosted Salmon",
       isNew: true,
-      oldPrice: 2,
-      price: 1,
+      oldPrice: 5.95,
+      price: 4.95,
     },
     {
       id: 4,
       img: "",
       img2: "",
-      title: "",
+      title: "Marigold Starfire Mix",
       isNew: true,
-      oldPrice: 2,
-      price: 1,
+      oldPrice: 5.95,
+      price: 4.95,
     },
   ];
 
   return (
     <div className="featuredProducts">
       <div className="top">
-        <h1>{type} Products</h1>
+        <h1>{type} Popular Products</h1>
         <p>
           Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do
           eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sit
