f205888c1f99f05ca3ac246a5544b9ef9133160b
Kyra Henningson <kyrahenningson@hotmail.com>
2023-01-20 13:08:59 -0800

    install stripe, make post call to /orders

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,7 +1,9 @@
 import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 import { useDispatch, useSelector } from "react-redux";
 import "./Cart.scss";
-import { removeItem, resetCart } from "../../redux/cartReducer"
+import { removeItem, resetCart } from "../../redux/cartReducer";
+import { makeRequest } from "../../makeRequest";
+import { loadStripe } from "@stripe/stripe-js";
 
 export default function Cart() {
   const products = useSelector((state) => state.cart.products);
@@ -10,7 +12,27 @@ export default function Cart() {
   const totalPrice = () => {
     let total = 0;
     products.forEach((item) => (total += item.quantity * item.price));
-    return total.toFixed(2)
+    return total.toFixed(2);
+  };
+
+  const stripePromise = loadStripe(
+    "pk_test_51Lu6wxKsfkXh0Y5Xv9pm1Dbsqgzf7vl8WticTCvZJq8HF422pp6TM4C9Q8HHaBCMj7IvTx1R5GFdBW7XH8NrJ6kd00ONfZTRQ1"
+  );
+
+  const handlePayment = async () => {
+    try {
+      const stripe = await stripePromise;
+
+      const res = await makeRequest.post("/orders", {
+        products,
+      });
+
+      await stripe.redirectToCheckout({
+        sessionId: res.data.stripeSession.id,
+      });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
@@ -28,15 +50,20 @@ export default function Cart() {
               {item.quantity} x ${item.price}
             </div>
           </div>
-          <DeleteOutlinedIcon className="delete" onClick={() => dispatch(removeItem(item.id))}/>
+          <DeleteOutlinedIcon
+            className="delete"
+            onClick={() => dispatch(removeItem(item.id))}
+          />
         </div>
       ))}
       <div className="total">
         <span>SUBTOTAL</span>
         <span>${totalPrice()}</span>
       </div>
-      <button>PROCEED TO CHECKOUT</button>
-      <span className="reset" onClick={() => dispatch(resetCart())}>Empty Cart</span>
+      <button onClick={handlePayment}>PROCEED TO CHECKOUT</button>
+      <span className="reset" onClick={() => dispatch(resetCart())}>
+        Empty Cart
+      </span>
     </div>
   );
 }
